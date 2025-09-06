a30a9cbc08f78ea55f6128e98ead7205509995fa
radenat <calistanafisaa@gmail.com>
2025-09-06 10:09:24 +0700

    Add files via upload

---
+++ assets/js/style.js
@@ -0,0 +1,38 @@
+
+    onload = () => {
+      const c = setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+
+        const titles = ('I LOVE YOU, CEDRIC').split('');
+        const titleElement = document.getElementById('title');
+        let index = 0;
+
+ function play() {
+          //Link Audio Bisa Diganti
+          var audio = new Audio('anchor.mp3');
+          audio.play();
+        }
+
+        function appendTitle() {
+          if (index < titles.length) {
+            titleElement.innerHTML += titles[index];
+            index++;
+            setTimeout(appendTitle, 300); // 300ms delay per huruf
+          } else {
+            // Setelah teks utama selesai, tambahkan subtitle kecil
+            const subtitle = document.createElement('p');
+            subtitle.innerText = "i love you more than words can express, so will you be mine? (Zefdran)";
+            subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
+            subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
+            subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
+
+            // Menambahkan subtitle di bawah #title
+            titleElement.appendChild(subtitle);
+          }
+        }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
