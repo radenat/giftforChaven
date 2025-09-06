3b129554b3b9586885181ad3e159403ef0c3946a
radenat <calistanafisaa@gmail.com>
2025-09-06 10:25:11 +0700

    Update style.js

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('I LOVE YOU, CEDRIC').split('');
+        const titles = ('I LOVE YOU').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "i love you more than words can express, so will you be mine? (Zefdran)";
+            subtitle.innerText = "Will you be mine?";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
